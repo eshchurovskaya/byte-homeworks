5d9e6184a2581b8b6f228cbd21759efe75d725e4
eshchurovskaya <eshchurovska@gmail.com>
2022-05-13 15:02:13 +0200

    add homeworks

---
+++ homework2/script.js
@@ -0,0 +1,21 @@
+let userName = 'Natalia';
+console.log(userName);
+
+let userSurname = 'Yeshchurovska';
+console.log(userSurname);
+
+let userAge = '29';
+console.log(userAge);
+
+
+let newUser;
+let shoppingList;
+let isUserOnline;
+let salaryEmployer;
+let coordinatesMouse;
+
+let firstWord = 'Hello World';
+console.log(firstWord);
+
+let userNickname = 'Tina';
+console.log('userNickname', userNickname);
