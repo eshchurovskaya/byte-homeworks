9eddc2af10871e5d681930e8aedf3f7b6e41a164
eshchurovskaya <eshchurovska@gmail.com>
2022-06-20 01:37:01 +0200

    homework3

---
+++ homework3.1/script.js
@@ -0,0 +1,50 @@
+// let firstNumber = prompt('Введите первое число');
+// let secondNumber = prompt('Введите второе число');
+
+// if (firstNumber > secondNumber) {
+//     alert(`Число ${firstNumber} больше, чем число ${secondNumber}`);
+// } else if (firstNumber < secondNumber) {
+//     alert(`Число ${firstNumber} меньше, чем число ${secondNumber}`);
+// } else if (firstNumber = secondNumber) {
+//     alert(`Число ${firstNumber} равно числу ${secondNumber}`);
+// };
+
+
+// let firstNumber = prompt('Введите первое число');
+// let secondNumber = prompt('Введите второе число');
+
+// let operation = prompt('Введите операцию');
+
+// let sum = firstNumber + secondNumber;
+// let min = firstNumber - secondNumber;
+// let multiply = firstNumber * secondNumber;
+// let division = firstNumber / secondNumber;
+
+// switch (operation){
+//     case '+':
+//         alert ('результат : ' + sum);
+//         break;
+//     case '-':
+//         alert ('результат : ' + min);
+//         break;
+//     case '*':
+//         alert ('результат : ' + multiply);
+//         break;
+//     case '/':
+//         alert ('результат : ' + division);
+//         break;
+// };
+
+
+
+
+//  let visitorAge = Number(prompt('Сколько вам лет?'));
+//  let parentsPermission = confirm('У вас есть разрешение от родителей?');
+
+// if ( visitorAge >= 18 && visitorAge <= 60) {
+//     alert ('доступ разрешен');
+// } else if ( (visitorAge >= 12 && visitorAge < 18) && parentsPermission || (visitorAge > 60 && visitorAge < 80) && parentsPermission) {
+//     alert ('доступ разрешен');
+// }  else {
+//     alert ('доступ запрещен');
+// };
