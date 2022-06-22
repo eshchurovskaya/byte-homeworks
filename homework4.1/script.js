4c44259978bd1a9ec45ed57ff3b7e263da2d9737
eshchurovskaya <eshchurovska@gmail.com>
2022-06-23 00:59:24 +0200

    homework4

---
+++ homework4.1/script.js
@@ -0,0 +1,39 @@
+// function sum(a, b){
+//     return a+b
+// };
+//  let result = sum (10, 8);
+//  console.log (result);
+
+//  let result1 = sum(5, 5);
+//  console.log (result1);
+
+
+
+// const summa = (a, b) => a + b;
+// const res2 = summa (1, 1);
+// console.log(res2);
+
+
+
+
+// let visitorName = prompt('Как вас зовут?');
+// let visitorAge = prompt ('Сколько Вам лет?');
+
+// function greeting () {
+//     if (visitorAge >= 30){
+//         alert (`Здавствуйте, ${visitorName}`);
+//     } else {
+//         alert (`Привет, ${visitorName}`);
+//     }
+// };
+
+// greeting ();
+
+
+
+function degreeNumber (a, b){
+    return a**b
+};
+
+let result3 = degreeNumber (5, 3);
+console.log(result3);
